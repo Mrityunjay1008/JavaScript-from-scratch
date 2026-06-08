c8744eca1d88eaa45b1957badef7dd92a78b43ab
Mrityunjay1008 <sharmagopal1306@gmail.com>
2026-06-08 11:30:10 +0530

    Operators

---
+++ 04_Operation/main.js
@@ -0,0 +1,91 @@
+let a =10
+let b = 20
+
+//Arithmetic Operators
+
+console.log(a + b) // addition
+console.log(a - b) // subtraction
+console.log(a * b) // multiplication
+console.log(a / b) // division
+console.log(a % b) // modulus
+console.log(a ** b) // power
+
+//Bitwise Operators
+a = 10
+b = 20
+
+console.log(a & b) // bitwise AND
+console.log(a | b) // bitwise OR
+console.log(a ^ b) // bitwise XOR
+console.log(~a) // bitwise NOT
+
+//Logical Operators
+a = 10
+b = 20
+c = 30
+d = 40
+e = 50
+f = 60
+g = 70
+h = 80
+i = 90
+j = 100
+
+console.log(a && b) // logical AND
+console.log(a || b) // logical OR
+console.log(!a) // logical NOT
+console.log(a == b) // equal to
+console.log(a != b) // not equal to
+console.log(a > b) // greater than
+console.log(a < b) // less than
+console.log(a >= b) // greater than or equal to
+console.log(a <= b) // less than or equal to
+
+//Assignment Operators
+a = 10
+b = 20
+c = 30
+d = 40
+e = 50
+f = 60
+g = 70
+h = 80
+i = 90
+j = 100
+
+let sum = a + b + c + d + e + f + g + h + i + j
+let average = sum / 10
+let remainder = a % b
+let power = a ** b
+let bitwiseAND = a & b
+let bitwiseOR = a | b
+let bitwiseXOR = a ^ b
+let bitwiseNOT = ~a
+let logicalAND = a && b
+let logicalOR = a || b
+let logicalNOT = !a
+let equalTo = a == b
+let notEqualTo = a != b
+let greaterThan = a > b
+let lessThan = a < b
+let greaterThanOrEqualTo = a >= b
+let lessThanOrEqualTo = a <= b
+
+console.log(sum)
+console.log(average)
+console.log(remainder)
+console.log(power)
+console.log(bitwiseAND)
+console.log(bitwiseOR)
+console.log(bitwiseXOR)
+console.log(bitwiseNOT)
+console.log(logicalAND)
+console.log(logicalOR)
+console.log(logicalNOT)
+console.log(equalTo)
+console.log(notEqualTo)
+console.log(greaterThan)
+console.log(lessThan)
+console.log(greaterThanOrEqualTo)
+console.log(lessThanOrEqualTo)
+                       
