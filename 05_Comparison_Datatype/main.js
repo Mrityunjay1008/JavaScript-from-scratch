3f629a05ab0e961be0bc308074263c80f178e1a5
Mrityunjay1008 <sharmagopal1306@gmail.com>
2026-06-08 11:57:16 +0530

    Datatype Comparison

---
+++ 05_Comparison_Datatype/main.js
@@ -0,0 +1,12 @@
+//This is the tricky part of JavaScript
+
+console.log("2">1); //true
+console.log("2"<1); //false
+
+console.log(null==undefined); //true
+console.log(null==null); //true
+console.log(null==0); //false
+console.log(null>0); //false
+console.log(null<0); //false
+
+// you should always compare same datatype
